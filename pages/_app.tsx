47f00738a2a4d34a65ed16ef9376ff6df83300c2
Mohamad Radisha <pr0ph0z23@gmail.com>
2023-09-01 18:55:01 +0700

    init: Add prototype

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,59 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState, useEffect } from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
+import Layout from "./layout";
+import { WagmiConfig, configureChains, createConfig, sepolia } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { publicProvider } from "wagmi/providers/public";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [sepolia],
+    [publicProvider()]
+  );
+  const config = createConfig({
+    autoConnect: true,
+    connectors: [new InjectedConnector({ chains })],
+    publicClient,
+    webSocketPublicClient,
+  });
+
+  return (
+    <>
+      <Head>
+        <title>PBT</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={"dark"}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{ colorScheme }}
+        >
+          <WagmiConfig config={config}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </WagmiConfig>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 }
