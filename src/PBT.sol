3b02b55e06629bb2ed715ffc5d6121a2850f67eb
Mohamad Radisha <pr0ph0z23@gmail.com>
2023-09-09 21:09:23 +0700

    refactor(contract): Update conventional naming for `_tokenchips`

--- src/PBT.sol
+++ src/PBT.sol
@@ -15,6 +15,7 @@ error BlockNumberTooOld();
 error ChipHasBeenMinted();
 error UnauthorizedToMint();
 error MismatchArrayLength();
+error UpdatingChipForUnsetChipMapping();
 
 contract PBT is ERC721A, IPBT, Ownable {
     using ECDSA for bytes32;
@@ -24,14 +25,14 @@ contract PBT is ERC721A, IPBT, Ownable {
         uint256 tokenId;
     }
 
-    mapping(address => TokenChip) private tokenChips;
+    mapping(address => TokenChip) _tokenChips;
 
     constructor() ERC721A("PBT", "PBT") {}
 
     function tokenIdFor(
         address chipAddress
     ) external view override returns (uint256) {
-        uint256 tokenId = tokenChips[chipAddress].tokenId;
+        uint256 tokenId = _tokenChips[chipAddress].tokenId;
         if (tokenId == 0) revert NoMintedTokenId();
 
         return tokenId;
@@ -47,7 +48,7 @@ contract PBT is ERC721A, IPBT, Ownable {
         bytes32 signedHash = keccak256(payload).toEthSignedMessageHash();
         address chipAddress = signedHash.recover(signature);
 
-        return tokenChips[chipAddress].tokenId == tokenId;
+        return _tokenChips[chipAddress].tokenId == tokenId;
     }
 
     function transferTokenWithChip(
@@ -77,13 +78,13 @@ contract PBT is ERC721A, IPBT, Ownable {
             signatureFromChip,
             blockNumberUsedInSig
         );
-        TokenChip storage tokenChip = tokenChips[chipAddress];
+        TokenChip storage tokenChip = _tokenChips[chipAddress];
 
         if (tokenChip.tokenId == 0 && tokenChip.chipAddress == address(0))
             revert UnauthorizedToMint();
         if (tokenChip.tokenId != 0) revert ChipHasBeenMinted();
 
-        tokenChips[chipAddress].tokenId = _nextTokenId();
+        _tokenChips[chipAddress].tokenId = _nextTokenId();
         _mint(msg.sender, 1);
         emit PBTMint(_nextTokenId() - 1, msg.sender);
     }
@@ -93,7 +94,7 @@ contract PBT is ERC721A, IPBT, Ownable {
     ) external onlyOwner {
         for (uint256 i = 0; i < chipAddresses.length; ) {
             address chipAddress = chipAddresses[i];
-            tokenChips[chipAddress] = TokenChip({
+            _tokenChips[chipAddress] = TokenChip({
                 chipAddress: chipAddress,
                 tokenId: 0
             });
@@ -113,14 +114,14 @@ contract PBT is ERC721A, IPBT, Ownable {
 
         for (uint256 i = 0; i < oldChipAddresses.length; ) {
             address oldChipAddress = oldChipAddresses[i];
-            TokenChip memory tokenChip = tokenChips[oldChipAddress];
+            TokenChip memory tokenChip = _tokenChips[oldChipAddress];
             address newChipAddress = newChipAddresses[i];
-            tokenChips[newChipAddress] = TokenChip({
+            _tokenChips[newChipAddress] = TokenChip({
                 chipAddress: newChipAddress,
                 tokenId: tokenChip.tokenId
             });
 
-            delete tokenChips[oldChipAddress];
+            delete _tokenChips[oldChipAddress];
             unchecked {
                 i++;
             }
@@ -136,7 +137,7 @@ contract PBT is ERC721A, IPBT, Ownable {
             signatureFromChip,
             blockNumberUsedInSig
         );
-        uint256 tokenId = tokenChips[chipAddress].tokenId;
+        uint256 tokenId = _tokenChips[chipAddress].tokenId;
 
         if (tokenId == 0) revert NoMintedTokenId();
 
