cc9e282a419f302e7c2c27ad4219ee868fbd7dce
Mohamad Radisha <pr0ph0z23@gmail.com>
2023-09-10 18:38:31 +0700

    chore: Redirect `/` to `/mint` as the default page

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/mint",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
