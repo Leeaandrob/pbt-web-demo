a8198bfb6ad52605f7b3e3e8ab718f0f29828640
Leandro Barbosa <leandrobar93@gmail.com>
2024-01-04 02:40:57 -0300

    fix: abi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ export default function App(props: AppProps) {
 
   const { chains, publicClient, webSocketPublicClient } = configureChains(
     [sepolia],
-    [publicProvider()]
+    [publicProvider()],
   );
 
   const { connectors } = getDefaultWallets({
